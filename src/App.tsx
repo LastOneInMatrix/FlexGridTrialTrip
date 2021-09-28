07a65434b4f33b9f2b97c0684d95613dc4cb7dac
Ash Barsov <a89875239469@gmail.com>
2021-09-28 17:36:40 +0300

    --flex direction --flex items --flex inline and simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={style.App}>
+      <header className={style.AppHeader}>
+          <div>Home</div>
+          <div>Search</div>
+          <div>Logout</div>
       </header>
+            <div className={style.flexContainer}>
+                <div className={style.flexItem + ' ' + style.item1}>Aside</div>
+                <div className={style.flexItem + ' ' + style.item2}>Main</div>
+                <div className={style.flexItem + ' ' + style.item3}>Aside</div>
+                {/*<div className={style.flexItem + ' ' + style.item4}>4</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item5}>5</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item6}>6</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item7}>7</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item8}>8</div>*/}
+            </div>
     </div>
   );
 }
