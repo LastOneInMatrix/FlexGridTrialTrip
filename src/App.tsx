cb05361099870ce8706184bbe92c4180a1dddc8b
Ash Barsov <a89875239469@gmail.com>
2021-09-29 22:16:32 +0300

    --ширина элементов по главной оси - задать ширину всем одну и расставить по местам flex-grow при росте и flex-shrink при уменьшении места

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
                 <div className={style.flexItem + ' ' + style.item1}>1</div>
                 <div className={style.flexItem + ' ' + style.item2}>2</div>
                 <div className={style.flexItem + ' ' + style.item3}>3</div>
-                <div className={style.flexItem + ' ' + style.item4}>4</div>
-                <div className={style.flexItem + ' ' + style.item5}>5</div>
-                <div className={style.flexItem + ' ' + style.item6}>6</div>
-                <div className={style.flexItem + ' ' + style.item7}>7</div>
-                <div className={style.flexItem + ' ' + style.item8}>8</div>
+                {/*<div className={style.flexItem + ' ' + style.item4}>4</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item5}>5</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item6}>6</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item7}>7</div>*/}
+                {/*<div className={style.flexItem + ' ' + style.item8}>8</div>*/}
             </div>
     </div>
   );
