c9a7a5622d65aa801d15447da00ced0318957a5d
Ash Barsov <a89875239469@gmail.com>
2021-09-29 21:50:39 +0300

    -- align-content выравнивание как и justify-content только для поперечной оси

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ function App() {
                 <div className={style.flexItem + ' ' + style.item2}>2</div>
                 <div className={style.flexItem + ' ' + style.item3}>3</div>
                 <div className={style.flexItem + ' ' + style.item4}>4</div>
-                {/*<div className={style.flexItem + ' ' + style.item5}>5</div>*/}
-                {/*<div className={style.flexItem + ' ' + style.item6}>6</div>*/}
-                {/*<div className={style.flexItem + ' ' + style.item7}>7</div>*/}
-                {/*<div className={style.flexItem + ' ' + style.item8}>8</div>*/}
+                <div className={style.flexItem + ' ' + style.item5}>5</div>
+                <div className={style.flexItem + ' ' + style.item6}>6</div>
+                <div className={style.flexItem + ' ' + style.item7}>7</div>
+                <div className={style.flexItem + ' ' + style.item8}>8</div>
             </div>
     </div>
   );
